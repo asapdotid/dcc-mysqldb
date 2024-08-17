80909b205f3b309ab01fb55ad97388491b07cbb0
Asapdotid <asapdotid@gmail.com>
2024-08-17 23:21:46 +0700

    ⚡️ IMPROVE: update utility script

--- src/script/utility-db.sh
+++ src/script/utility-db.sh
@@ -29,9 +29,36 @@ WHITE=$(echo -en '\001\033[01;37m\002')
 DB_HOST=127.0.0.1
 DB_PORT=3306
 
+function show_databases() {
+    echo ""
+    echo "${LCYAN}Backup database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
+    read -r -p "Database username: " DB_USERNAME
+    # Disable echo
+    stty -echo
+    read -r -s -p "Database password: " DB_PASSWORD
+    # Turn echo back on
+    stty echo
+    printf "\n"
+    until mysql -u"$DB_USERNAME" -p"$DB_PASSWORD" -h"$DB_HOST" -P"$DB_PORT" -e ";"; do
+        # Disable echo
+        stty -echo
+        read -r -s -p "Can't connect, please retry: " DB_PASSWORD
+        # Turn echo back on
+        stty echo
+        printf "\n"
+    done
+    echo "${LMAGENTA}Show databases...${RESTORE}"
+    i=1
+    for DB in $(mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" -N -B -e "SHOW DATABASES"); do
+        num=$((i++))
+        echo "Database ${GREEN}$num${RESTORE}: ${YELLOW}$DB${RESTORE}"
+    done
+    echo "${GREEN}Show databases successfully${RESTORE}"
+    echo ""
+}
 function backup_database() {
     echo ""
-    echo "${CYAN}Backup database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
+    echo "${LCYAN}Backup database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
     read -r -p "Database username: " DB_USERNAME
     # Disable echo
     stty -echo
@@ -51,6 +78,7 @@ function backup_database() {
     if [ ! -d "/tmp/backup" ]; then
         mkdir /tmp/backup
     fi
+    echo "${LMAGENTA}Backup database...${RESTORE}"
     mysqldump --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" \
         --max-allowed-packet=1024M --single-transaction --lock-tables --set-gtid-purged=OFF --no-data \
         "$T" | gzip -c >"/tmp/backup/$DB_NAME-$T.sql.gz"
@@ -59,7 +87,7 @@ function backup_database() {
 }
 function restore_database() {
     echo ""
-    echo "${CYAN}Restore database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
+    echo "${LCYAN}Restore database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
     read -r -p "Database username: " DB_USERNAME
     # Disable echo
     stty -echo
@@ -76,17 +104,19 @@ function restore_database() {
         stty echo
         printf "\n"
     done
+    echo "${LMAGENTA}Restore database...${RESTORE}"
+    i=1
     for T in $(mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" -N -B -e "SHOW TABLES FROM $DB_NAME"); do
         num=$((i++))
-        echo "Restore data table $num: $DB_NAME.$T"
-        mysql --max_allowed_packet=1024M --default-character-set=utf8 --binary-mode=1 --user=$DB_USERNAME --password=$DB_PASSWORD --host=$DB_HOST --port=$DB_PORT $DB_NAME <"/tmp/backup/pm-tables/$T.sql"
+        echo "Restore data table ${GREEN}$num${RESTORE}: ${YELLOW}$DB_NAME.$T${RESTORE}"
+        mysql --max_allowed_packet=1024M --default-character-set=utf8 --binary-mode=1 --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" "$DB_NAME" <"/tmp/backup/tables/$DB_NAME-$T.sql"
     done
     echo "${GREEN}Database $DB_NAME restored successfully${RESTORE}"
     echo ""
 }
 function trucate_database() {
     echo ""
-    echo "${CYAN}Cleaning database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
+    echo "${LCYAN}Cleaning database on ${DB_HOST}:${DB_PORT}:${RESTORE} "
     read -r -p "Database username: " DB_USERNAME
     # Disable echo
     stty -echo
@@ -103,9 +133,14 @@ function trucate_database() {
         stty echo
         printf "\n"
     done
-    echo "Mysql host $DB_HOST, port $DB_PORT, cleaning data table $num: $DB_NAME.$T"
-    mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" -N -B -e "TRUNCATE TABLE $DB_NAME.$T"
-    echo "${GREEN}Database $DB_NAME backup created successfully${RESTORE}"
+    echo "${LMAGENTA}Truncate database...${RESTORE}"
+    i=1
+    for T in $(mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" -N -B -e "SHOW TABLES FROM $DB_NAME"); do
+        num=$((i++))
+        echo "Truncate data table ${GREEN}$num${RESTORE}: ${YELLOW}$DB_NAME.$T${RESTORE}"
+        mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" --host="$DB_HOST" --port="$DB_PORT" -N -B -e "TRUNCATE TABLE $DB_NAME.$T"
+    done
+    echo "${GREEN}Truncate database $DB_NAME successfully${RESTORE}"
     echo ""
 }
 
@@ -113,28 +148,33 @@ function trucate_database() {
 menu() {
     echo -ne "
 Actions menu for setup database:
-${GREEN}1)${RESTORE} Backup Database
-${GREEN}2)${RESTORE} Restore Database
-${GREEN}3)${RESTORE} Truncate Database
+${GREEN}1)${RESTORE} Show Databases
+${GREEN}2)${RESTORE} Backup Database
+${GREEN}3)${RESTORE} Restore Database
+${GREEN}4)${RESTORE} Truncate Database
 ${GREEN}0)${RESTORE} Exit
 ${BLUE}Choose an option:${RESTORE} "
     read -r a
     case $a in
     1)
-        backup_database
+        show_databases
         menu
         ;;
     2)
-        restore_database
+        backup_database
         menu
         ;;
     3)
+        restore_database
+        menu
+        ;;
+    4)
         trucate_database
         menu
         ;;
     0) exit 0 ;;
     *)
-        echo -e "$(ColorRed 'Wrong option.')"
+        echo -e "${RED}Wrong option.${RESTORE}"
         menu
         ;;
     esac
